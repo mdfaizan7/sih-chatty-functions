5dfaf39db40957ce25fb3b838b9b323176b847c0
Mohammad Faizan Azim <faizaanazim@gmail.com>
2020-03-08 14:01:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ const { postMessage } = require("./handlers/messages");
 
 const { signUp, login } = require("./handlers/users");
 
+// conversation routes
 app.post("/conversation", FBAuth, createConv);
-app.get("/conversation", FBAuth, getConversations);
+app.get("/conversations", FBAuth, getConversations);
 app.get("/conversation/:ConvId", getConversation);
 
+// user routes
 app.post("/signup", signUp);
 app.post("/login", login);
 
+// message routes
 app.post("/message/:ConvId", FBAuth, postMessage);
 
 exports.api = functions.https.onRequest(app);
